0ac51f736c9538c0ab81a6a5a0043737239f6bca
Murph <shmilyclxy@163.com>
2019-09-29 18:01:48 +0800

    fix: update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>react-ssr</h1>
+        <p>content</p>
+      </div>
+    )
+  }
+}
